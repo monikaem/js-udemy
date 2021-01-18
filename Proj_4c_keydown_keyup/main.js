ec98c396332fc541667c217ecfd09b3e387ed2f5
Monika <monika.machula94@gmail.com>
2021-01-18 13:50:45 +0100

    modified files

--- Proj_4c_keydown_keyup/main.js
+++ Proj_4c_keydown_keyup/main.js
@@ -5,14 +5,16 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;// ustawiam kolor poczatkowy na szary
 
 const changeColor = (event) => {   // funkcja zmieniajaca kolor w zaleznosci od tego ktory klawisz wciskamy
-    if (event.keyCode === 38) {  // 38 to keycode strzalki w gore
+    if (event.keyCode === 38 && red < 255) {  // 38 to keycode strzalki w gore
         red++;
         green++;
         blue++;
-    } else if (event.keyCode === 40){ // to keycode strzalki w dol
+
+    } else if (event.keyCode === 40 && red >= 0){ // to keycode strzalki w dol
         red--;
         green--;
         blue--;
+
     }
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`; //ustawiam nowy kolor
 }
@@ -20,19 +22,13 @@ const changeColor = (event) => {   // funkcja zmieniajaca kolor w zaleznosci od 
 // const changeColor = (event) => {
 //     switch (event.keyCode){
 //         case 38:
-//             console.log('Strzalka up')
-//             red++;
-//             green++;
-//             blue++;
-//             break;
+//             document.body.style.backgroundColor = `rgb(${red < 255 ? red++ : red }, ${green < 255 ? green++ : green}, ${blue < 255 ? blue++ : blue}`;
+//                 break;
 //         case 40:
-//             console.log('strzalka down')
-//             red--;
-//             green--;
-//             blue--;
-//         break;
+//             document.body.style.backgroundColor = `rgb(${red >= 0 ? red-- : red}, ${green >= 0 ? green-- : green}, ${blue >= 0 ? blue-- : blue}`;
+//                 break;
 //     }
-//     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+//
 // }
 window.addEventListener('keydown', changeColor);
 
