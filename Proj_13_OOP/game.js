f21d10b8e703f957b0c826e34548d2a131595987
Monika <monika.machula94@gmail.com>
2021-01-27 11:03:22 +0100

    added comments to file game.js

--- Proj_13_OOP/game.js
+++ Proj_13_OOP/game.js
@@ -3,7 +3,7 @@ class Game {
         this.stats = new Statistics();
         this.wallet = new Wallet(start);
 
-        document.getElementById('start').addEventListener('click', this.startGame.bind(this));
+        document.getElementById('start').addEventListener('click', this.startGame.bind(this)); //pobieramy wszystkie elementy
         this.spanWallet = document.querySelector('.panel span.wallet');
         this.boards = [...document.querySelectorAll('div.color')];
         this.inputBid = document.querySelector('#bid');
@@ -11,41 +11,41 @@ class Game {
         this.spanGames = document.querySelector('.score span.number');
         this.spanWins = document.querySelector('.score span.win');
         this.spanLosses = document.querySelector('.score span.loss');
-        this.render();
+        this.render();   //poczatkowy wyglad strony
     }
 
     render(colors = ['gray', 'gray', 'gray'], money = this.wallet.getWalletValue(), result = '', stats = [0, 0, 0], bid = 0, wonMoney = 0) {
         this.boards.forEach((color, index) => {
-                color.style.backgroundColor = colors[index]
+                color.style.backgroundColor = colors[index]   //ustawienie kolor tablic zgodnie z losowaniem
             }
         )
-        this.spanWallet.textContent = money;
+        this.spanWallet.textContent = money;  // ustawienie zawartosci pozostalych pieniedzy
         if (result) {
             result = `Wygrałeś ${wonMoney}$. `
         } else if (!result && result !== "") {
             result = `Przegrałeś ${bid}$. `
         }
-        this.spanResult.textContent = result;
-        this.spanGames.textContent = stats[0];
-        this.spanWins.textContent = stats[1];
-        this.spanLosses.textContent = stats[2];
-        this.inputBid.value = '';
+        this.spanResult.textContent = result;  //wynik
+        this.spanGames.textContent = stats[0]; //liczba gier
+        this.spanWins.textContent = stats[1];  //liczba wygranych
+        this.spanLosses.textContent = stats[2];  // liczba przegranych
+        this.inputBid.value = ''; // zerowanie zawartosci inputu po grze
     }
 
     startGame() {
-        if (this.inputBid.value < 1) return alert('Kwota, którą chcesz grać jest za mała!');
+        if (this.inputBid.value < 1) return alert('Kwota, którą chcesz grać jest za mała!'); //sprawdzenie czy wpisana kwota jest prawidlowa
         const bid = Math.floor(this.inputBid.value);
-        if (!this.wallet.checkCanPlay(bid)){
+        if (!this.wallet.checkCanPlay(bid)){   //sprawdzenie czy jest wystarczajaco srodkow do gry
             return alert('Masz za mało środków lub podana została nieprawidłowa wartość');
         }
-        this.wallet.changeWallet(bid, '-');
-        this.draw = new Draw();
-        const colors = this.draw.getDrawResult();
-        const win = Result.checkWinner(colors);
-        const wonMoney = Result.moneyWonInGame(win, bid);
-        this.wallet.changeWallet(wonMoney, '+');
-        this.stats.addGameToStats(win, bid);
-        this.render(colors, this.wallet.getWalletValue(), win, this.stats.showGamesStats(), bid, wonMoney);
+        this.wallet.changeWallet(bid, '-'); // zmiana zawartosci portfela po wpisaniu inputu
+        this.draw = new Draw(); // losowanie
+        const colors = this.draw.getDrawResult(); // pobranie wyniku losowania i przypisanie do zmiennej colors
+        const win = Result.checkWinner(colors); //sprawdzenie kto wygral
+        const wonMoney = Result.moneyWonInGame(win, bid); // sprawdzenie ile wygrano
+        this.wallet.changeWallet(wonMoney, '+'); // zmiana zawartosci portfela po grze
+        this.stats.addGameToStats(win, bid); //dodanie statystyk
+        this.render(colors, this.wallet.getWalletValue(), win, this.stats.showGamesStats(), bid, wonMoney); // ponowne wczytanie strony z nowymi wartosciami
     }
 }
 
